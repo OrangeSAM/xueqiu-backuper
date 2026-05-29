3ad6b9a49b135e14078d8d65dacdb82fc56a62f8
SamLiu <amazing_sam@outlook.com>
2026-05-29 20:47:34 +0800

    docs: add xueqiu API reference files
    Captured fetch() calls and JSON responses for timeline and
    comment APIs, used as reference for building the scraper.

---
+++ comment.js
@@ -0,0 +1,20 @@
+fetch("https://xueqiu.com/statuses/v3/comments.json?id=391803135&type=4&size=20&max_id=-1", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cache-control": "no-cache",
+    "elastic-apm-traceparent": "00-07d4855faee178d2a7ccc0b48a8ed739-6e19253cc4d74583-00",
+    "pragma": "no-cache",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Chromium\";v=\"148\", \"Google Chrome\";v=\"148\", \"Not/A)Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "cookie": "cookiesu=431763392340994; device_id=704cce2aa6d682f3500e81e7c0d177db; smidV2=202511172312226402ba3312b18e5ac2c9cf1cb31c50020027df27caeb43e80; u=8097712233; s=bi1dhq1d0f; bid=7e3a520fddc497313a1122f925c3aeea_mi8zhreg; xq_is_login=1; _ga_M96YC1H9CR=GS2.1.s1779173260$o1$g0$t1779173260$j60$l0$h0; _ga=GA1.2.1672754997.1779173260; aliyungf_tc=b05d2986c2ae06bddf7aefecc1def958cd3e39598ae96e838ac4087f818a9025; HMACCOUNT=1C8E4C5AF3F51411; Hm_lvt_1db88642e346389874251b5a1eded6e3=1779440423; xq_a_token=9570029e6dd009676a47c5ab5aa4f781190e8940; xqat=9570029e6dd009676a47c5ab5aa4f781190e8940; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOjgwOTc3MTIyMzMsImlzcyI6InVjIiwiZXhwIjoxNzgyNTU1NTY5LCJjdG0iOjE3Nzk5NjM1Njk5NDgsImNpZCI6ImQ5ZDBuNEFadXAifQ.eP35UG3bODlf2iL88gfaBf8vZTPCpScjIGRiI3_JuHIsmy5bxnIvqLgcNd_kvlFo8ViZ2vUdQr7M9qqnJf1Ph9O2IpDPxH6oeTHMkDSpTDtB5O1eMUeqxOLHQacnKy9F4tsThgYBYawZ0DQ6s0fLGP3jmWynlUUJftkt8mJ0UlY50_jyDJQyMsN3-sp12U9Ndt1wISzTAR6RdRYnFS27JfgH3wZ3Br08tLxg87MbwjBCP2U-Xcetnm2QZ1l7KwvWLKHudKkyeTuHVuogk2JG-pa4rn6_yFd7nLrjDTMxoi1pqCJKhA4oO7FcuS_478giePNBR9Wqpr_ehReiqJt9aQ; xq_r_token=e8c177ae2f09f396cf15588d53b102be2940aec4; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1780047152; .thumbcache_f24b8bbe5a5934237bbc0eda20c1b6e7=AQXgi1dofyG7NM4eMU6oDHdcOmrfapSFHF86owh1yH3qg5tuXthieWjJU5KCfHsIT0Iuf+IDHFgMNAP5EEckOQ%3D%3D",
+    "Referer": "https://xueqiu.com/4533843739/391803135"
+  },
+  "body": null,
+  "method": "GET"
+});
---
+++ timeline.js
@@ -0,0 +1,21 @@
+fetch("https://xueqiu.com/v4/statuses/user_timeline.json?page=1&user_id=4533843739&type=0&_=1780047045838", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cache-control": "no-cache",
+    "elastic-apm-traceparent": "00-3ca7757876377b18f935dbc329934614-121e0b2e6b525778-00",
+    "pragma": "no-cache",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Chromium\";v=\"148\", \"Google Chrome\";v=\"148\", \"Not/A)Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest",
+    "cookie": "cookiesu=431763392340994; device_id=704cce2aa6d682f3500e81e7c0d177db; smidV2=202511172312226402ba3312b18e5ac2c9cf1cb31c50020027df27caeb43e80; u=8097712233; s=bi1dhq1d0f; bid=7e3a520fddc497313a1122f925c3aeea_mi8zhreg; xq_is_login=1; _ga_M96YC1H9CR=GS2.1.s1779173260$o1$g0$t1779173260$j60$l0$h0; _ga=GA1.2.1672754997.1779173260; aliyungf_tc=b05d2986c2ae06bddf7aefecc1def958cd3e39598ae96e838ac4087f818a9025; HMACCOUNT=1C8E4C5AF3F51411; Hm_lvt_1db88642e346389874251b5a1eded6e3=1779440423; xq_a_token=9570029e6dd009676a47c5ab5aa4f781190e8940; xqat=9570029e6dd009676a47c5ab5aa4f781190e8940; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOjgwOTc3MTIyMzMsImlzcyI6InVjIiwiZXhwIjoxNzgyNTU1NTY5LCJjdG0iOjE3Nzk5NjM1Njk5NDgsImNpZCI6ImQ5ZDBuNEFadXAifQ.eP35UG3bODlf2iL88gfaBf8vZTPCpScjIGRiI3_JuHIsmy5bxnIvqLgcNd_kvlFo8ViZ2vUdQr7M9qqnJf1Ph9O2IpDPxH6oeTHMkDSpTDtB5O1eMUeqxOLHQacnKy9F4tsThgYBYawZ0DQ6s0fLGP3jmWynlUUJftkt8mJ0UlY50_jyDJQyMsN3-sp12U9Ndt1wISzTAR6RdRYnFS27JfgH3wZ3Br08tLxg87MbwjBCP2U-Xcetnm2QZ1l7KwvWLKHudKkyeTuHVuogk2JG-pa4rn6_yFd7nLrjDTMxoi1pqCJKhA4oO7FcuS_478giePNBR9Wqpr_ehReiqJt9aQ; xq_r_token=e8c177ae2f09f396cf15588d53b102be2940aec4; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1780047046; .thumbcache_f24b8bbe5a5934237bbc0eda20c1b6e7=zO5WKzRCcThgT80gyWf3oN+eKQaOpDSb9B8LxPKk4KzG2SsIW6lsOAG9cgVl5ZaYrfj/0TUueYhCzKv9YzaMTw%3D%3D",
+    "Referer": "https://xueqiu.com/u/4533843739"
+  },
+  "body": null,
+  "method": "GET"
+});
